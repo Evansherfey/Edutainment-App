2b1398d502683bb9f47c80ad654bb097a5560b7c
Evan <evan.sherfey@stu.barren.kyschools.us>
2024-12-17 08:17:02 -0600

    Initial Commit

---
+++ Edutainment App/Edutainment_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edutainment_AppApp.swift
+//  Edutainment App
+//
+//  Created by Student on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Edutainment_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
